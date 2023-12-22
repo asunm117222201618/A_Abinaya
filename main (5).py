031276744fc6094b695c0cac15770560bfa04e14
asunm117222201618 <144885431+asunm117222201618@users.noreply.github.com>
2023-12-22 13:55:47 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,43 @@
+class Students:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+def sort_students(student_lists):
+  sorted_students = sorted(student_lists,key=lambda student: student.cgpa, reverse=True)
+  return sorted_students
+
+students = [
+  Students("Priya", "A123", 7.8),
+  Students("Mithu", "A124", 8.9),
+  Students("Rithi", "A215", 9.1),
+  Students("Kavi", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,student.roll_number, student.cgpa))class Students:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+def sort_students(student_lists):
+  sorted_students = sorted(student_lists,key=lambda student: student.cgpa, reverse=True)
+  return sorted_students
+
+students = [
+  Students("Priya", "A123", 7.8),
+  Students("Mithu", "A124", 8.9),
+  Students("Rithi", "A215", 9.1),
+  Students("Kavi", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,student.roll_number, student.cgpa))
